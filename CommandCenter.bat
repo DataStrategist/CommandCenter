37a3ef657f4db2f2727c9ad8bca6dac482662880
Amit <mexindian@gmail.com>
2016-05-29 15:49:14 +0000

    Update CommandCenter.bat

--- CommandCenter.bat
+++ CommandCenter.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\R\R-3.2.2\bin\x64\R.exe"  --vanilla --slave CMD BATCH "D:\Dropbox\My projects\Command Center\command.R"
+"C:\Program Files\R\R-3.2.2\bin\x64\R.exe"  --vanilla --slave CMD BATCH "PATH TO THE FOLDER HOSTING THE COMMAND CENTER\command.R"
